71527faa287cd949c92fd2b0b4ad7d8faad411f0
Adarsh Mohite <mohiteadarsh15@gmail.com>
2026-01-20 20:57:16 +0530

    Time: 1 ms (71.65%), Space: 54 MB (76.02%) - LeetHub

---
+++ 0274-h-index/0274-h-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    citations.sort((a, b) => a - b);
+
+    let n = citations.length;
+
+    for (let i = 0; i < n; i++) {
+        let h = n - i;   // number of papers left
+        if (citations[i] >= h) {
+            return h;
+        }
+    }
+    return 0;
+};
